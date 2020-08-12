05e9a9cdc96535569c9219c329197598087f6074
Quentin Read <quentin.read@gmail.com>
2020-08-12 10:07:10 -0400

    update savetable script

--- 2_vis/savetables.R
+++ 2_vis/savetables.R
@@ -1,3 +1,22 @@
+table_costbreakdown_alternate <- dat_costbreakdown_alternate %>%
+  ungroup %>%
+  bind_rows(dat_totalcost_alternate %>% mutate(cost_type = 'total') %>% mutate_if(is.numeric, ~ round(./1e6))) %>%
+  mutate_if(is.numeric, round) %>%
+  mutate(cost_with_quantiles = paste0(q50, ' (', q05, '; ', q95, ')')) %>%
+  select(intervention, cost_type, cost_with_quantiles) %>%
+  pivot_wider(names_from = cost_type, values_from = cost_with_quantiles, values_fill = list(cost_with_quantiles = "--")) %>%
+  select(intervention, initial_raw, initial_annualized, annual, total) %>%
+  setNames(c('Intervention', 'Initial capital cost', 'Annualized initial cost', 'Annual cost', 'Total (annualized + annual) cost'))
+
+table_netcost_alternate <- dat_netcost_alternate %>%
+  mutate_if(is.numeric, ~ round(./1e6)) %>%
+  mutate(cost_with_quantiles = paste0(q50, ' (', q05, '; ', q95, ')')) %>%
+  select(intervention, name, cost_with_quantiles) %>%
+  pivot_wider(names_from = name, values_from = cost_with_quantiles, values_fill = list(cost_with_quantiles = "--")) %>%
+  select(intervention, total_cost, averted_food_purchase, net_cost) %>%
+  arrange(intervention) %>%
+  setNames(c('Intervention', 'Cost', 'Averted food purchase', 'Net cost or savings'))
+
 cbind(table_costbreakdown_alternate, table_netcost_alternate[,3:4]) %>%
   write_csv('~/google_drive/SESYNC Food Waste/MS3_Interventions/figs/table2.csv')
 
